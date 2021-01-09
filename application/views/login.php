994ffd558bbaa67f0f19b5f9aaecafb7f855a5ee
Steven Villarosa <steven.s.villarosa@gmail.com>
2021-01-09 11:07:06 +0800

    Adding validation to user login

--- application/views/login.php
+++ application/views/login.php
@@ -3,24 +3,25 @@
         <div class="row">
             <div class="col-md-4"></div>
             <div class="col-md-4">
-                <section id="content" >			
-                    <?php echo validation_errors(); ?>
+                <section id="content" >
                     <?php echo form_open(); ?>
                         <div class="mb-3">
                             <label for="username" class="form-label">Username</label>
                             <input type="text" class="form-control" id="username" name="username" maxLength="50">
+                            <?php echo form_error('username'); ?>
                         </div>
                         <div class="mb-3">
                             <label for="password" class="form-label">Password</label>
                             <input type="password" class="form-control" id="password" name="password" maxLength="50">
+                            <?php echo form_error('password'); ?>
                         </div>
                         <div class="text-center">
                             <button type="submit" class="btn btn-primary">Login</button>
                         </div>
-                    </form>								
+                    </form>
                 </section>
             </div>
             <div class="col-md-4"></div>
         </div>
-    </div>	
+    </div>
 </div>
