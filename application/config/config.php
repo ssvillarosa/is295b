3e3acfdb686e4444a934acd42bac886b96280372
Steven Villarosa <steven.s.villarosa@gmail.com>
2021-01-21 14:43:48 +0800

    Set staging environment configuration

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'https://m2mj.m2mjhrconsulting.com/steven/is295b';
 
 /*
 |--------------------------------------------------------------------------
