87870efe39a74d220549a095784aa493bf678ed4
Steven Villarosa <steven.s.villarosa@gmail.com>
2021-02-02 19:37:06 +0800

    Delete user from details page.

--- application/views/user/detailsView.php
+++ application/views/user/detailsView.php
@@ -1,3 +1,33 @@
+<script>
+    
+    $(document).ready(function() {
+        // Create a post request to delete user/s.
+        $("#UserDetailsDeleteForm").submit(function(e){
+            e.preventDefault();
+            console.log("hello");
+            $.post('<?php echo site_url('user/delete') ?>', 
+            $(this).serialize(),
+            function(data) {
+                hideDialog();
+                if(data.trim() === "Success"){
+                    showToast("Deleted Successfully.",3000);
+                    setTimeout(function(){
+                        window.location.replace("<?php echo site_url('user/userList') ?>");
+                    }, 1000);
+                    return;
+                }
+                showToast("Error occurred.",3000);
+            }).fail(function() {
+                showToast("Error occurred.",3000);
+            });
+        });
+    });
+    
+    // Displays the dialog to confirm deletion.
+    function showDeleteDialog(){
+        $("#delete_dialog").fadeIn();
+    }
+</script>
 <div id="user-details-page" class="user-details-page">
     <div class="container">
         <div class="row justify-content-center">
@@ -64,13 +94,35 @@
                             <input type="text" value="<?php echo $user->address; ?>" class="form-control" id="address" name="address" maxLength="50">
                             <?php echo form_error('address'); ?>
                         </div>
-                        <div class="text-center">
-                            <button type="submit" class="btn btn-primary">Save</button>
-                            <a href="<?php echo site_url('user/userList') ?>" class="btn btn-secondary">Cancel</a>
+                        <div class="d-flex justify-content-between">
+                            <div class="text-left">
+                                <a href="<?php echo site_url('user/userList') ?>" class="btn btn-success">View Logs</a>
+                                <button type="button" class="btn btn-danger" onclick="showDeleteDialog()">Delete</button>
+                            </div>
+                            <div class="text-right">
+                                <button type="submit" class="btn btn-primary">Save</button>
+                                <a href="<?php echo site_url('user/userList') ?>" class="btn btn-secondary">Cancel</a>
+                            </div>
                         </div>
                     </form>
                 </section>
             </div>
         </div>
     </div>	
+</div>
+
+<div class="m2mj-dialog" id="delete_dialog">
+    <div class="dialog-background"></div>
+    <div class="m2mj-modal-content">
+        <?php echo form_open('user/delete','id="UserDetailsDeleteForm"'); ?>        
+        <input type="hidden" name="delUserIds" id="delUserIds" value="<?php echo $user->id; ?>"/>
+            <div class="modal-body">
+                <strong class="modal-text">Are you sure you want to delete?</strong>
+            </div>
+            <div class="modal-footer p-2">
+              <button type="button" class="btn btn-secondary m2mj-dialog-close">Close</button>
+              <button type="submit" class="btn btn-danger" id="delete_confirm">Delete</button>
+            </div>
+        </form>
+    </div>
 </div>
