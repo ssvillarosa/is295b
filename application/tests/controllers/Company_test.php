0004e00b952d3da32067d58d4b90fc4c0df86784
Steven Villarosa <steven.s.villarosa@gmail.com>
2021-02-06 12:20:53 +0800

    Added unit test case for company view.

--- application/tests/controllers/Company_test.php
+++ application/tests/controllers/Company_test.php
@@ -34,17 +34,23 @@ class Company_test extends TestCase{
         $page1 = $this->request('GET','company/companyList?rowsPerPage=1');
         $rowCount = substr_count($page1,'company-row-item');
         $this->assertEquals(1, $rowCount);
-        $this->assertContains('id="company-1"', $output);
-        $this->assertNotContains('id="company-2"', $output);
+        $this->assertContains('id="company-1"', $page1);
+        $this->assertNotContains('id="company-2"', $page1);
         
         $page2 = $this->request('GET','company/companyList?rowsPerPage=1&currentPage=2');
         $rowCount = substr_count($page2,'company-row-item');
         $this->assertEquals(1, $rowCount);
-        $this->assertContains('id="company-2"', $output);
-        $this->assertNotContains('id="company-1"', $output);
+        $this->assertContains('id="company-2"', $page2);
+        $this->assertNotContains('id="company-1"', $page2);
         
         $newPage = $this->request('GET','company/companyList?rowsPerPage=3');
         $rowCount = substr_count($newPage,'company-row-item');
         $this->assertEquals(3, $rowCount);
+    }
+    
+    public function test_view(){
+        $page = $this->request('GET','company/view?id=1');
+        $this->assertContains('ABC Corp', $page);
+        $this->assertContains('Steven Suanuqe', $page);
     }
 }
