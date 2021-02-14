dc6757d534ee7570be2e84b0df93c086a0c86c80
Steven Villarosa <steven.s.villarosa@gmail.com>
2021-02-14 14:43:00 +0800

    Added unit test case for update job order.

--- application/tests/controllers/JobOrder_test.php
+++ application/tests/controllers/JobOrder_test.php
@@ -37,4 +37,30 @@ class JobOrder_test extends TestCase{
         $this->assertContains('id="skill-3"', $output);
         $this->assertContains('id="skill-4"', $output);
     }
+    
+    public function test_update(){
+        $postData = [
+            'jobOrderId' => '2',
+            'title' => 'Software',
+            'company_id' => '3',
+            'min_salary' => '11111',
+            'max_salary' => '1111110000',
+            'employment_type' => '2',
+            'status' => '2',
+            'job_function' => '11111',
+            'requirement' => '11111111111',
+            'skillIds' => '1,4',
+            'skillNames' => 'Javascript,Indexing',
+            'yearsOfExperiences' => '8,1',
+            'location' => '111111',
+            'slots_available' => '10',
+            'priority_level' => '10',
+        ];
+        $this->request('POST','job_order/update',$postData);
+        $output = $this->request('GET','job_order/view?id=2');
+        $this->assertContains('value="Software"', $output);
+        $this->assertContains('id="skill-1"', $output);
+        $this->assertContains('id="skill-4"', $output);
+        $this->assertNotContains('id="skill-3"', $output);
+    }
 }
