a2636a916b341188f8de064692bb97a8f49449ee
Steven Villarosa <steven.s.villarosa@gmail.com>
2021-03-03 21:21:56 +0800

    Make UI components smaller in job order and applicant modules.

--- application/views/applicant/detailsView.php
+++ application/views/applicant/detailsView.php
@@ -25,7 +25,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <section id="content" >
-                    <h5 class="mb-3">Applicant details: </h5>
+                    <h5 class="mb-1">Applicant details: </h5>
                     <?php if(isset($success_message)): ?>
                         <div class="alert alert-success" role="alert">
                             <?php echo $success_message; ?>
@@ -40,48 +40,48 @@
                     <?php echo form_open('applicant/update','id="updateApplicantForm"'); ?>
                         <input type="hidden" value="<?php echo $applicant->id; ?>" id="applicantId" name="applicantId">
                         <div class="form-row">
-                            <div class="col-md-6 mb-3">
+                            <div class="col-md-6 mb-1">
                                 <label for="last_name" class="form-label">Last Name</label>
-                                <input type="text" value="<?php echo $applicant->last_name; ?>" class="form-control" id="last_name" name="last_name" maxLength="255" required>
+                                <input type="text" value="<?php echo $applicant->last_name; ?>" class="form-control form-control-sm" id="last_name" name="last_name" maxLength="255" required>
                                 <?php echo form_error('last_name','<div class="alert alert-danger">','</div>'); ?>
                             </div>
-                            <div class="col-md-6 mb-3">
+                            <div class="col-md-6 mb-1">
                                 <label for="first_name" class="form-label">First Name</label>
-                                <input type="text" value="<?php echo $applicant->first_name; ?>" class="form-control" id="first_name" name="first_name" maxLength="255" required>
+                                <input type="text" value="<?php echo $applicant->first_name; ?>" class="form-control form-control-sm" id="first_name" name="first_name" maxLength="255" required>
                                 <?php echo form_error('first_name','<div class="alert alert-danger">','</div>'); ?>
                             </div>
                         </div>
                         <div class="form-row">
-                            <div class="col-md-6 mb-3">
+                            <div class="col-md-6 mb-1">
                                 <label for="email" class="form-label">Email</label>
-                                <input type="email" value="<?php echo $applicant->email; ?>" class="form-control" id="email" name="email" maxLength="255">
+                                <input type="email" value="<?php echo $applicant->email; ?>" class="form-control form-control-sm" id="email" name="email" maxLength="255">
                                 <?php echo form_error('email','<div class="alert alert-danger">','</div>'); ?>
                             </div>
-                            <div class="col-md-6 mb-3">
+                            <div class="col-md-6 mb-1">
                                 <label for="primary_phone" class="form-label">Primary Phone</label>
-                                <input type="text" value="<?php echo $applicant->primary_phone; ?>" class="form-control" id="primary_phone" name="primary_phone" maxLength="255">
+                                <input type="text" value="<?php echo $applicant->primary_phone; ?>" class="form-control form-control-sm" id="primary_phone" name="primary_phone" maxLength="255">
                                 <?php echo form_error('primary_phone','<div class="alert alert-danger">','</div>'); ?>
                             </div>
                         </div>
                         <div class="form-row">
-                            <div class="col-md-6 mb-3">
+                            <div class="col-md-6 mb-1">
                                 <label for="secondary_phone" class="form-label">Secondary Phone</label>
-                                <input type="text" value="<?php echo $applicant->secondary_phone; ?>" class="form-control" id="secondary_phone" name="secondary_phone" maxLength="255">
+                                <input type="text" value="<?php echo $applicant->secondary_phone; ?>" class="form-control form-control-sm" id="secondary_phone" name="secondary_phone" maxLength="255">
                                 <?php echo form_error('secondary_phone','<div class="alert alert-danger">','</div>'); ?>
                             </div>
-                            <div class="col-md-6 mb-3">
+                            <div class="col-md-6 mb-1">
                                 <label for="work_phone" class="form-label">Work Phone</label>
-                                <input type="text" value="<?php echo $applicant->work_phone; ?>" class="form-control" id="work_phone" name="work_phone" maxLength="255">
+                                <input type="text" value="<?php echo $applicant->work_phone; ?>" class="form-control form-control-sm" id="work_phone" name="work_phone" maxLength="255">
                                 <?php echo form_error('work_phone','<div class="alert alert-danger">','</div>'); ?>
                             </div>
                         </div>
                         <div class="form-row">
                             <div class="col-md-12 mb-1">
                                 <label for="address" class="form-label">Address</label>
-                                <input type="text" value="<?php echo $applicant->address; ?>" class="form-control" id="address" name="address" maxLength="255">
+                                <input type="text" value="<?php echo $applicant->address; ?>" class="form-control form-control-sm" id="address" name="address" maxLength="255">
                                 <?php echo form_error('address','<div class="alert alert-danger">','</div>'); ?>
                             </div>
-                            <div class="custom-control custom-checkbox mr-sm-2  mb-3">
+                            <div class="custom-control custom-checkbox mr-sm-2  mb-1">
                                 <input type="checkbox" class="custom-control-input" id="can_relocate" name="can_relocate" value="1"
                                        <?php if($applicant->can_relocate != "0") echo "checked"; ?>>
                                 <label class="custom-control-label" for="can_relocate">Can relocate</label>
@@ -89,38 +89,38 @@
                             </div>
                         </div>
                         <div class="form-row">
-                            <div class="col-md-6 mb-3">
+                            <div class="col-md-6 mb-1">
                                 <label for="current_employer" class="form-label">Current Employer</label>
-                                <input type="text" value="<?php echo $applicant->current_employer; ?>" class="form-control" id="current_employer" name="current_employer" maxLength="255">
+                                <input type="text" value="<?php echo $applicant->current_employer; ?>" class="form-control form-control-sm" id="current_employer" name="current_employer" maxLength="255">
                                 <?php echo form_error('current_employer','<div class="alert alert-danger">','</div>'); ?>
                             </div>
-                            <div class="col-md-6 mb-3">
+                            <div class="col-md-6 mb-1">
                                 <label for="source" class="form-label">Source</label>
-                                <input type="text" value="<?php echo $applicant->source; ?>" class="form-control" id="source" name="source" maxLength="255">
+                                <input type="text" value="<?php echo $applicant->source; ?>" class="form-control form-control-sm" id="source" name="source" maxLength="255">
                                 <?php echo form_error('source','<div class="alert alert-danger">','</div>'); ?>
                             </div>
                         </div>
                         <div class="form-row">
-                            <div class="col-md-12 mb-3">
+                            <div class="col-md-12 mb-1">
                                 <label for="best_time_to_call" class="form-label">Best Time to Call</label>
-                                <input type="text" value="<?php echo $applicant->best_time_to_call; ?>" class="form-control" id="best_time_to_call" name="best_time_to_call" maxLength="255">
+                                <input type="text" value="<?php echo $applicant->best_time_to_call; ?>" class="form-control form-control-sm" id="best_time_to_call" name="best_time_to_call" maxLength="255">
                                 <?php echo form_error('best_time_to_call','<div class="alert alert-danger">','</div>'); ?>
                             </div>
                         </div>
                         <div class="form-row">
-                            <div class="col-md-6 mb-3">
+                            <div class="col-md-6 mb-1">
                                 <label for="current_pay" class="form-label">Current Pay</label>
-                                <input type="number" value="<?php if($applicant->current_pay != "0") echo $applicant->current_pay; ?>" class="form-control" id="current_pay" name="current_pay" maxLength="255">
+                                <input type="number" value="<?php if($applicant->current_pay != "0") echo $applicant->current_pay; ?>" class="form-control form-control-sm" id="current_pay" name="current_pay" maxLength="255">
                                 <?php echo form_error('current_pay','<div class="alert alert-danger">','</div>'); ?>
                             </div>
-                            <div class="col-md-6 mb-3">
+                            <div class="col-md-6 mb-1">
                                 <label for="desired_pay" class="form-label">Desired Pay</label>
-                                <input type="number" value="<?php if($applicant->desired_pay != "0") echo $applicant->desired_pay; ?>" class="form-control" id="desired_pay" name="desired_pay" maxLength="255">
+                                <input type="number" value="<?php if($applicant->desired_pay != "0") echo $applicant->desired_pay; ?>" class="form-control form-control-sm" id="desired_pay" name="desired_pay" maxLength="255">
                                 <?php echo form_error('desired_pay','<div class="alert alert-danger">','</div>'); ?>
                             </div>
                         </div>
                         <div class="form-row">
-                            <div class="col-md-12 mb-3">
+                            <div class="col-md-12 mb-1">
                                 <label for="skills" class="form-label">Skills</label>
                                 <div id="skills">
                                     <?php foreach($applicant_skills as $applicant_skill){
