93597328df4f74d5a167208af7be6d88937d6948
Steven Villarosa <steven.s.villarosa@gmail.com>
2021-04-25 11:40:56 +0800

    Added unit test case for assigned to me.

---
+++ application/tests/controllers/AdminDashboard_test.php
@@ -0,0 +1,34 @@
+<?php
+
+class AdminDashboard_test extends TestCase{
+    
+    public static function setUpBeforeClass(){
+        parent::setUpBeforeClass();
+
+        // Reset user database.
+        $CI =& get_instance();
+        $CI->load->library('Seeder');
+        $CI->seeder->call('JobOrderSeeder');
+        $CI->seeder->call('ApplicantSeeder');
+        $CI->seeder->call('PipelineSeeder');
+    }
+    
+    public function setUp(){        
+        // Login as admin.
+        $this->request(
+            'POST',
+            'auth/login',
+            [
+                'username' => 'admin',
+                'password' => 'adminpw',
+            ]
+        );
+    }
+    
+    public function test_getAssignedToMe(){
+        $output = $this->request('GET','admin_dashboard/getAssignedToMe');
+        $this->assertContains('Steven Villarosa', $output);
+        $this->assertContains('Software Developer', $output);
+        $this->assertContains('Theresa San Jose', $output);
+    }
+}
